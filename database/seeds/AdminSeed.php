cc9ee99bd61cbc4a6b9a53ecdc46f2df45135ebc
Vekisa <vmosorinski@gmail.com>
2018-12-21 12:04:05 +0100

    added admin seeder

---
+++ database/seeds/AdminSeed.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Enum\UserRole;
+use Illuminate\Database\Seeder;
+
+class AdminSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+            'role' => UserRole::ADMIN
+        ]);
+    }
+}
